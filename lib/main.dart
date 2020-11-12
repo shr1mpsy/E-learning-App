a2c570e2a775988ee38b8592b4fda645cf4eb0b1
shr1mpsy <74312310+shr1mpsy@users.noreply.github.com>
2020-11-12 21:46:47 +0100

    Code cleanup + App update

--- lib/main.dart
+++ lib/main.dart
@@ -13,23 +13,23 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
   var index = 0;
-  bool alreadyPressed = true;
 
   int qindex = 0;
   int qtotalScore = 0;
 
   void reset() {
-    qindex = 0;
-    qtotalScore = 0;
-    alreadyPressed = true;
+    setState(() {
+      qindex = 0;
+      qtotalScore = 0;
+    });
+
   }
 
   void antwort(int score) {
-    if (alreadyPressed){
-      qtotalScore += score;
-      qindex = qindex + 1;
-      alreadyPressed = false;
-    }
+      setState(() {
+        qtotalScore += score;
+        qindex = qindex + 1;
+      });
   }
 
   //Eine Seite weiter
@@ -50,10 +50,6 @@ class MyAppState extends State<MyApp> {
     print(index);
   }
 
-  void quiz(){
-    setState(() {alreadyPressed = true;});
-  }
-
   void nm(){
     setState(() {
       index = 0;
@@ -61,44 +57,35 @@ class MyAppState extends State<MyApp> {
     });
   }
 
+
   @override
   Widget build(BuildContext context) {
-    if (index < 4) {
-      return MaterialApp(
-        home: Scaffold(
+    return MaterialApp(
+      home: Scaffold(
           appBar: AppBar(
-            title: Text("Cloud Computing"),
+            title: Text("Cloud-Computing"),
           ),
-          body: Column(
-            children: <Widget>[
-              Texte(index),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  RaisedButton(child: Text("<<Zurück"), onPressed: antwortbw),
-                  RaisedButton(child: Text("Weiter>>"), onPressed: antwortfw),
-                ],
-              ),
-            ],
-          ),
-        ),
-      );
-    }
-    if (index == 4){
-      return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("Cloud Computing"),
-          ),
-          body: Column(
-            children: <Widget>[
-              Quiz(qindex, qtotalScore, reset, antwort),
-              RaisedButton(child: Text("Weiter"), onPressed: quiz),
-              FlatButton(onPressed: nm, child: Text("Lektion neu starten"))
-            ],
-          ),
-        ),
-      );
-    }
+          body: index < 4
+              ? Column(
+                  children: <Widget>[
+                    Texte(index),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        RaisedButton(child: Text("<<Zurück"), onPressed: antwortbw),
+                        RaisedButton(child: Text("Weiter>>"), onPressed: antwortfw),
+                      ],
+                    ),
+                  ],
+                )
+
+              : Column(
+                  children: <Widget>[
+                  Quiz(qindex, qtotalScore, reset, antwort),
+                  FlatButton(onPressed: nm, child: Text("Lektion neu starten"))
+                  ],
+                )
+      ),
+    );
   }
 }
